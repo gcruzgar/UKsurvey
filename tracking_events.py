fc5456684430f421e7c889faee5ab4ca22dff52d
Gonzalo <43855433+gcruzgar@users.noreply.github.com>
2018-10-22 13:13:49 +0100

    Update tracking_events.py
    put code into a function, added random generation if individual identifier is not specified.

--- tracking_events.py
+++ tracking_events.py
@@ -4,26 +4,37 @@ date started: 19/10/18
 """
 
 import pandas as pd 
+from random import randint
 
 filename1 = "a_indresp.tab"
 filename2 = "b_indresp.tab"
 
-df1 = pd.read_csv(filename1, sep='\t')
-df2 = pd.read_csv(filename2, sep='\t')
+#pidp = 
 
-pidp = 1639357682
+def track_event(filename1, filename2, pidp = None):
 
-mstat_1 = df1.loc[df1['pidp'] == pidp, 'a_mlstat'].item() 
-mstat_2 = df2.loc[df2['pidp'] == pidp, 'b_mlstat'].item() 
+    df1 = pd.read_csv(filename1, sep='\t')
+    df2 = pd.read_csv(filename2, sep='\t')
 
-m_evol = [mstat_1, mstat_2]
+    while pidp == None:
+        r = randint(0, len(df1))
+        if r in df2['pidp']:
+            pidp = df1['pidp'][r]
 
-marital_status = {1: "single, never married/in civil partnership", 2: "married",
-    3: "civil partner", 4: "separated from spouse",
-    5: "divorced", 6: "widowed",
-    7: "separated from civil partner", 8: "former civil partner",
-    9: "surviving civil partner", -1: "don't know",
-    -9: "missing", -8: "inapplicable",
-    -7: "proxy", -2: "refusal"}
+    mlstat_1 = df1.loc[df1['pidp'] == pidp, 'a_mlstat'].item() 
+    mlstat_2 = df2.loc[df2['pidp'] == pidp, 'b_mlstat'].item() 
 
-print("Individual %d went from %s to %s" % (pidp, marital_status[mstat_1], marital_status[mstat_2]))
+    m_evol = [mlstat_1, mlstat_2]
+
+    marital_status = {1: "single, never married/in civil partnership", 2: "married",
+        3: "civil partner", 4: "separated from spouse",
+        5: "divorced", 6: "widowed",
+        7: "separated from civil partner", 8: "former civil partner",
+        9: "surviving civil partner", -1: "don't know",
+        -9: "missing", -8: "inapplicable",
+        -7: "proxy", -2: "refusal"}
+
+    print("Individual %d went from %s to %s" % (pidp, marital_status[mlstat_1], marital_status[mlstat_2]))
+    return m_evol
+
+m_evol = track_event(filename1, filename2)
