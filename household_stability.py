fc99cf3bf482320c237e1138b86fee2a656fcf53
Gonzalo <43855433+gcruzgar@users.noreply.github.com>
2018-10-25 15:05:12 +0100

    Update household_stability.py
    changed so code runs for all waves.
    added a count limit because the code takes a long time to run

--- household_stability.py
+++ household_stability.py
@@ -4,50 +4,59 @@ date started: 24/10/18
 
 This script generates a list of households for every individual present in all waves.
 
+Note: takes a long time to run.
+
 Things to do:
--update to all waves
 -load hidp file and compare number of members in hh
 """
 import pandas as pd 
 from random import sample
 
 # name of files/waves to compare
-filename1 = "a_indresp.tab"
-filename2 = "b_indresp.tab"
+filelist = ["a_indresp.tab", "b_indresp.tab", "c_indresp.tab", 
+"d_indresp.tab", "e_indresp.tab", "f_indresp.tab", "g_indresp.tab"]
 
-def wave_intercept(filename1, filename2):
-    """ Check which individuals from a wave are present in the next wave. """
-     
-    df1 = pd.read_csv(filename1, sep='\t')
-    df2 = pd.read_csv(filename2, sep='\t')
+def longevity(filelist):
+    """ List of individuals that are present in all waves """
+    pidp_dic = {}
+    wc=1
+    for name in filelist:
+        print("Loading wave %d data..." % wc)
+        df = pd.read_csv(name, sep='\t')
+        pidp_dic[str(wc)] = df['pidp']
+        wc+=1
+    
+    id_int = set(pidp_dic['2']).intersection(set(pidp_dic['1']))
+    for n in range(3,len(filelist)):
+        id_int = set(pidp_dic[str(n)]).intersection(id_int)
+    return id_int
 
-    df1_id = df1['pidp']
-    df2_id = df2['pidp']
-
-    #intercept between two lists:
-    id_intercept = set(df2_id).intersection(set(df1_id))
-    return list(id_intercept)
-
-id_intercept = wave_intercept(filename1,filename2)
-
-filelist = ["a_indresp.tab", "b_indresp.tab"]
+if 'id_list' not in vars():
+    print("Generating id_list...")
+    id_list = longevity(filelist)
+else:
+    print("id_list already present")
 
 def household_id_list(filelist, pidp):
     """ For a set of waves, obtain a list of household IDs belonging to the same individual. """
 
     hidp_list = []
-    wn = {1:'a', 2:'b'}
+    wn = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g'}
     c=1
     for name in filelist:
         df = pd.read_csv(name, sep='\t')
-        if pidp in df['pidp'].values:
-            kword = wn[c]+'_hidp'
-            hidp = df.loc[df['pidp'] == pidp, kword].values
-            hidp_list.extend(hidp)
+        kword = wn[c]+'_hidp'
+        hidp = df.loc[df['pidp'] == pidp, kword].values
+        hidp_list.extend(hidp)
         c+=1
     return hidp_list
 
+c = 0
 house_dic = {}
-for pidp in id_intercept:
-    house_dic[str(pidp)] = household_id_list(filelist, pidp)
-    print("Individual %d ... done" % pidp)
+for pidp in id_list:
+    c +=1
+    if c > 10:
+        break
+    else:
+        house_dic[str(pidp)] = household_id_list(filelist, pidp)
+        print("Individual %d ... done" % pidp)
