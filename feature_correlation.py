bc945305e6629d9a13981cb80a00c6051695681d
Gonzalo <43855433+gcruzgar@users.noreply.github.com>
2018-11-06 13:28:40 +0000

    Update feature_correlation.py
    turned into a function

--- feature_correlation.py
+++ feature_correlation.py
@@ -8,14 +8,19 @@ This script generates a table of correlations for the selected features.
 import pandas as pd 
 import numpy as np 
 
-cols = ['type', 'tenure', 'beds', 'rooms', 'size']
+def feature_correlation(wave):
+    cols = ['type', 'tenure', 'beds', 'rooms', 'size']
+    df = pd.read_csv(wave + '_hhresp.tab', sep = '\t')
+
+    feature_df = df[[wave+'_hhtype_dv', wave+'_tenure_dv', wave+'_hsbeds', wave+'_hsrooms', wave+'_hhsize']]
+    feature_df.columns = cols
+
+    fc = feature_df.corr()
+    return fc
 
 wn = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g'}
-wave = wn[1] # select wave
 
-df = pd.read_csv(wave + '_hhresp.tab', sep = '\t')
-
-feature_df = df[[wave+'_hhtype_dv', wave+'_tenure_dv', wave+'_hsbeds', wave+'_hsrooms', wave+'_hhsize']]
-feature_df.columns = cols
-
-feature_df.corr()
+feature_dict = {}
+for wave in wn.values():
+    print("Loading wave %s" % wave)
+    feature_dict[wave]= feature_correlation(wave)
