20411aef7bddc33e0c6dd6090b42a93cfa36e2f3
gcruzgar <g.cruz@leeds.ac.uk>
2018-11-21 16:46:57 +0000

    changed ratio of married to cohabiting

--- scripts/crosstabulation.py
+++ scripts/crosstabulation.py
@@ -38,8 +38,8 @@ def constrain(table, column, minval, maxval, shift=0):
 def contingency_table(wave):
 
     waveletter = chr(96+wave) # 1 -> "a" etc
-    #data = pd.read_csv(data_root_dir / ("UKDA-6614-tab/tab/ukhls_w" + str(wave)) / (waveletter + '_hhresp.tab'), sep = '\t')
-    data = pd.read_csv(data_root_dir / (waveletter+'_hhresp.tab'), sep ='\t')
+    data = pd.read_csv(data_root_dir / ("UKDA-6614-tab/tab/ukhls_w" + str(wave)) / (waveletter + '_hhresp.tab'), sep = '\t')
+    #data = pd.read_csv(data_root_dir / (waveletter+'_hhresp.tab'), sep ='\t')
     # hhsamp = pd.read_csv(data_root_dir / (waveletter+'_hhsamp.tab'), sep ='\t')
 
     # need to remove cases with one or more missing rooms/beds values *before* aggregating rooms
