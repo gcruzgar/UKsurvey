0aabc3a43104aabb47f05faad6cc241c2443a919
gcruzgar <g.cruz@leeds.ac.uk>
2018-11-23 10:53:37 +0000

    working on tracking households

--- scripts/hh_list.py
+++ scripts/hh_list.py
@@ -8,3 +8,16 @@ def hh_list():
 
     hidp_list.to_csv('data/xwave_hh_list.csv')
     return hidp_list
+
+def extract_var(wave, var_name):
+
+    waveletter = chr(96+wave) # 1 -> "a" etc
+    data = pd.read_csv('data/'+waveletter+'_hhresp.tab', sep ='\t')
+    hh_var = data[waveletter+var_name]
+    return hh_var
+
+def track_hh():
+    # the idea is to track households over time
+    # obtain a dataframe with values of the chosen variable for every household
+    # read the hidps for a hh and extract their corresponding values of hh_var
+    pass
