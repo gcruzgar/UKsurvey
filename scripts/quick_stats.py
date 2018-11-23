8f24ff6942a5e4d042fabb15d3127901060977fd
gcruzgar <g.cruz@leeds.ac.uk>
2018-11-23 10:19:45 +0000

    added metadata for 5 dimensions

--- scripts/quick_stats.py
+++ scripts/quick_stats.py
@@ -13,6 +13,7 @@ df = pd.read_csv("data/ssm_hh_E06000047_OA11_2011.csv")
 
 dim_df = df[['LC4402_C_TENHUK11', 'LC4408_C_AHTHUK11', 'LC4404EW_C_SIZHUK11', 
     'LC4404EW_C_ROOMS', 'LC4405EW_C_BEDROOMS']]
+dim_df.columns = ['Tenure', 'Composition', 'Occupants', 'Rooms', 'Bedrooms']
 
 def plot_dist(dim):
 
@@ -31,3 +32,50 @@ def plot_dist(dim):
 
 for dim in dim_df.columns:
     plot_dist(dim)
+
+dim_metadata = {
+    "Tenure": {
+        "0": "All categories: Tenure",
+        "1": "Owned or shared ownership: Total",
+        "2": "Owned: Owned outright",
+        "3": "Owned: Owned with a mortgage or loan or shared ownership",
+        "4": "Rented or living rent free: Total",
+        "5": "Rented: Social rented",
+        "6": "Rented: Private rented or living rent free"
+    },
+
+    "Composition": {
+        "0": "All categories: Household type",
+        "1": "One person household",
+        "2": "Married or same-sex civil partnership couple household",
+        "3": "Cohabiting couple household",
+        "4": "Lone parent household",
+        "5": "Multi-person household"
+    },
+
+    "Occupants": {
+        "0": "All categories: Household size",
+        "1": "1 person in household",
+        "2": "2 people in household",
+        "3": "3 people in household",
+        "4": "4 or more people in household"
+    },
+
+    "Rooms": {
+        "0": "All categories: Number of rooms",
+        "1": "1 room",
+        "2": "2 rooms",
+        "3": "3 rooms",
+        "4": "4 rooms",
+        "5": "5 rooms",
+        "6": "6 or more rooms"
+    },
+
+    "Bedrooms": {
+        "0": "All categories: Number of bedrooms",
+        "1": "1 bedroom",
+        "2": "2 bedrooms",
+        "3": "3 bedrooms",
+        "4": "4 or more bedrooms"
+    }
+}
