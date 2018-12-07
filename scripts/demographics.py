28456bdc37ff3a32e9758559fd0bf3a1a289ec36
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-07 11:04:06 +0000

    updating comments

--- scripts/demographics.py
+++ scripts/demographics.py
@@ -26,16 +26,16 @@ def extract_var(wave, var_name):
 
 def track_hh(sex, waves, var_name, hidp_list):
 
-    hh_row = hidp_list.loc[hidp_list['sex'] == sex] # household the individual is a member off in each wave 
-
+    hh_row = hidp_list.loc[hidp_list['sex'] == sex] # only keep values with the required demographic
+    
     print("Extracting %s..." % var_name)
     
     track_vals = []
     for wave in waves:
         waveletter = chr(96+wave) # 1 -> "a" etc    
 
-        hh_var = extract_var(wave, var_name)
-           
+        hh_var = extract_var(wave, var_name) # extract required variable for all households
+
         w_val = hh_var.loc[hh_var[waveletter+'_hidp'] == hh_row[waveletter+'_hidp'].item(), waveletter+var_name].values #extract value for the hh at that wave
         if w_val.size == 0: #if the household wasn't present in a wave, set it's value to '-9'
             w_val = [-9]
