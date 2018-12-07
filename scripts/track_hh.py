8c5e3cab81b02da32d9aebc0360c4ba91e62fe89
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-07 10:38:45 +0000

    combining both for loops

--- scripts/track_hh.py
+++ scripts/track_hh.py
@@ -37,14 +37,14 @@ def track_hh(pidp, waves, var_name, hidp_list):
     hh_row = hidp_list.loc[hidp_list['pidp'] == pidp] # household the individual is a member off in each wave 
 
     print("Extracting %s..." % var_name)
-    hh_var_dict = {}    #pd.DataFrame()
-    for wave in waves:
-        hh_var = extract_var(wave, var_name)  #.set_index(waveletter+'_hidp')
-        hh_var_dict[wave] = hh_var
-
+    hh_var_dict = {}
     track_vals = []
     for wave in waves:
-        waveletter = chr(96+wave) # 1 -> "a" etc       
+        waveletter = chr(96+wave) # 1 -> "a" etc    
+
+        hh_var = extract_var(wave, var_name)
+        hh_var_dict[wave] = hh_var
+           
         val_df = hh_var_dict[wave] #variable values for a given wave
         w_val = val_df.loc[val_df[waveletter+'_hidp'] == hh_row[waveletter+'_hidp'].item(), waveletter+var_name].values #extract value for the hh at that wave
         if w_val.size == 0: #if the household wasn't present in a wave, set it's value to '-9'
