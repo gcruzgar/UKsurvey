499901634a6b421d7fe688715c09a60e0a7d26ed
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-13 14:57:48 +0000

    adding option to filter by age band

--- scripts/track_hrpid.py
+++ scripts/track_hrpid.py
@@ -44,7 +44,7 @@ def main ():
 
     # household ids
     hidp_df = pd.read_csv("data/xwaveid.tab", sep='\t')
-    hidp_df = hidp_df[['pidp', 'sex', 'a_hidp', 'b_hidp', 'c_hidp', 'd_hidp', 'e_hidp', 'f_hidp', 'g_hidp']]
+    hidp_df = hidp_df[['pidp', 'sex', 'birthy', 'a_hidp', 'b_hidp', 'c_hidp', 'd_hidp', 'e_hidp', 'f_hidp', 'g_hidp']]
     hrpid_df = var_dict[1][['a_hidp', 'a_hrpid']]
 
     # dataframe with reference person for each household only
@@ -56,10 +56,19 @@ def main ():
         print("sex: %s" % s_val)
         hidp_df = hidp_df.loc[(hidp_df['sex'] == s_val)]
 
-    print("Extracting variable data...\n")
+    # option to filter by age band
+    if args.ab:
+        l_band = args.ab[0]
+        u_band = args.ab[1]
+        print("\nage band: %d-%d" % (l_band, u_band))
+        hidp_df = hidp_df.loc[(hidp_df['birthy'] < (2009 - l_band))
+            & (hidp_df['birthy'] > (2009 - u_band))]
+        print("households inside age band: %d" % len(hidp_df))
+
+    print("\nExtracting variable data...")
 
     track_df = pd.DataFrame()
-    for pidp in hidp_df['pidp'].head(100):
+    for pidp in hidp_df['pidp'].head(200):
         track_df[pidp]  = track_hh(pidp, waves, var_name, hidp_df, var_dict)
     track_df = track_df.T
     track_df.columns = ['a'+var_name,  'b'+var_name, 'c'+var_name, 
@@ -69,12 +78,13 @@ def main ():
     # drop any households that were only present for 2 waves or less
     drop_index = track_df.index[(track_df == -9).sum(axis=1) >= 0.70*track_df.shape[1]]
     track_df.drop(drop_index, axis=0, inplace=True)
-    print(track_df.head(10))
+    print(track_df.head(20))
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", type=int, nargs='?', help="sex, 1 for male or 2 for female")
+    parser.add_argument("-s", type=int, nargs=1, help="sex, 1 for male or 2 for female")
+    parser.add_argument("-ab", type=int, nargs=2, help="age band. requires the lower and upper bounds, eg. 30 44")
     parser.add_argument("var_name", type=str, nargs='?', default='_hhsize',
         help="variable of interest to extract. must be in hhresp.tab. type without wave prefix 'w', e.g. _hhsize")        
     args = parser.parse_args()
