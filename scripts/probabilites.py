0de0046bb2581d296de0637c180aec1c0e925d3c
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-17 14:05:33 +0000

    updating prints

--- scripts/probabilites.py
+++ scripts/probabilites.py
@@ -50,7 +50,8 @@ def main ():
         if (aw_is.loc[i] == in_state).all():
             c+=1 
 
-    print('\nPercentage stable households: %.2f%%' % (c/len(aw_is.index)*100))
+    print('\nPercentage stable households: %.2f%%' % (c/len(aw_is)*100))
+    print('%d/%d households remained at value = %s' % (c, len(aw_is), in_state))
 
     return transition_df, t_perc_df
 
