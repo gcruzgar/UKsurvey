a4364bd1b996cdb7af641d489f57e0f5ba888421
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-17 11:45:54 +0000

    printing probabilities for transitions in every wave

--- scripts/probabilites.py
+++ scripts/probabilites.py
@@ -2,10 +2,7 @@
 import pandas as pd 
 import argparse
 
-
 def main ():
-
-    waves = [1,2,3,4,5,6,7]
 
     if args.var_name.startswith("_"): # variable to extract
         var_name = args.var_name    
@@ -19,20 +16,29 @@ def main ():
     print("Loading household data...\n")
     # household response data - only keep required variables (files are too big to store in memory)
     var_dict = {}
-    for wave in waves:
+    for wave in range(1,8):
+        
         waveletter = chr(96+wave) # 1 -> "a" etc
         data = pd.read_csv('data/'+waveletter+'_hhresp.tab', sep ='\t')
         var_dict[wave] = data[[waveletter+'_hrpid', waveletter+var_name]].set_index(waveletter+'_hrpid')
 
-    ij_df = pd.concat([var_dict[1], var_dict[2]], axis=1, join='inner')
+    t_dict = {}
+    for wave in range(1,7):
+        
+        ij_df = pd.concat([var_dict[wave], var_dict[wave+1]], axis=1, join='inner')
 
-    tdf = ij_df.loc[ij_df['a'+var_name] == in_state]
-    t = tdf['b'+var_name].value_counts()
+        w1 = chr(96+wave)
+        w2 = chr(97+wave)
 
-    t_perc = t/sum(t) * 100
-    print(t_perc)
+        tdf = ij_df.loc[ij_df[w1+var_name] == in_state]
+        t = tdf[w2+var_name].value_counts()
+        t_dict[wave] = t
+
+        t_perc = t/sum(t) * 100
+        print(t_perc)
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("var_name", type=str, nargs='?', default='_hhtype_dv',
         help="variable of interest to extract. must be in hhresp.tab. type without wave prefix 'w', e.g. _hhtype_dv")   
