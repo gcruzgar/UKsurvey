1ac091f11df7c78f59a4cad054481b2169937b48
gcruzgar <g.cruz@leeds.ac.uk>
2018-12-17 11:02:54 +0000

    adding argument parser for variable and initial value

--- scripts/probabilites.py
+++ scripts/probabilites.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import pandas as pd 
+import argparse
 
 def main ():
+    if args.var_name.startswith("_"): # variable to extract
+        var_name = args.var_name    
+    else:                             # catch variables without underscore
+        var_name = '_'+args.var_name
+    print("\nvariable: %s" % var_name)
 
-    var_name = '_hhtype_dv'
-    var_val = 3
+    in_state = args.in_state          # value of initial state
+    print("Initial state: %d\n" % in_state)
 
     adf  = pd.read_csv("data/a_hhresp.tab", sep ='\t')
     adf = adf[['a_hrpid', 'a'+var_name]].set_index('a_hrpid')
@@ -14,12 +20,18 @@ def main ():
 
     ij_df = pd.concat([adf, bdf], axis=1, join='inner')
 
-    tdf = ij_df.loc[ij_df['a'+var_name] == var_val]
+    tdf = ij_df.loc[ij_df['a'+var_name] == in_state]
     t = tdf['b'+var_name].value_counts()
 
     t_perc = t/sum(t) * 100
     print(t_perc)
 
 if __name__ == "__main__":
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("var_name", type=str, nargs='?', default='_hhtype_dv',
+        help="variable of interest to extract. must be in hhresp.tab. type without wave prefix 'w', e.g. _hhtype_dv")   
+    parser.add_argument("in_state", type=int, nargs='?', default = 3,
+        help="numerical value of initial state")     
+    args = parser.parse_args()
+     
     main()
