f593a418929776bcd2466422f85fb96f1a47ee74
gcruzgar <g.cruz@leeds.ac.uk>
2019-01-03 11:16:39 +0000

    rounding numbers to 2dp before plot

--- scripts/tpm.py
+++ scripts/tpm.py
@@ -106,7 +106,7 @@ def main ():
     print(tpm.round(2))
 
     # plot probabilities
-    ax = sns.heatmap(tpm, linewidth=.5, cmap="GnBu", annot=True, cbar_kws={'label':'Percentage (%)'})
+    ax = sns.heatmap(tpm.round(2), linewidth=.5, cmap="GnBu", annot=True, cbar_kws={'label':'Percentage (%)'})
     ax.set_title('Average Transition Probabilities - %s' % var_name[1:])
     plt.show()
 
